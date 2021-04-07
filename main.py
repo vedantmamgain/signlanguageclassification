e0a60f77974d098171969da9f0d067dda4bc548e
Vedant Mamgain <mamgainvedant@gmail.com>
2021-04-07 14:54:40 +0530

    Correctly loading the data

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 from pandas import read_csv
 import numpy as np
 import pandas as pd
+
+
 def load_file(filepath):
     dataframe = read_csv(filepath, header=None, delimiter=',')
     return dataframe.values
 
 
-def load_subject(sub_idx, sign, start, end):
+def load_subject(sub_idx, sign, start, end, idx1, idx2):
     start -= 1
     end -= 1
     filepath = str("Dataset/20_sign_language/" + "S" + str(sub_idx) + "_RA" + str(sign) + ".csv")
     # fix why n rows has an offset of 8
-    dataframe = read_csv(filepath, header=None, delimiter=',', skiprows=start, nrows=end - 8)
+    dataframe = read_csv(filepath, header=None, delimiter=',', skiprows=start, nrows=end - 8,
+                         usecols=[*range(idx1, idx2)])
+    #print(dataframe.values)
     return dataframe.values
 
 
-def load_rows(start, end, sign,sub_idx):
+def load_rows(start, end, sign, sub_idx):
     arr = []
-    arr.append(load_subject(sub_idx, sign, start, end))
+    # for emg right  3->5
+    arr.append(load_subject(sub_idx, sign, start[0], end[0], 3, 5))
+    # for accelo right  9->17
+    arr.append(load_subject(sub_idx, sign, start[1], end[1], 9, 17))
+    # for gyro right 27->35
+    arr.append(load_subject(sub_idx, sign, start[2], end[2], 27, 35))
+    # for emg left  6->8
+    arr.append(load_subject(sub_idx, sign, start[3], end[3], 6, 8))
+    # for accelo left 18->26
+    arr.append(load_subject(sub_idx, sign, start[4], end[4], 18, 26))
+    # for gyro left  36->44
+    arr.append(load_subject(sub_idx, sign, start[5], end[5], 36, 44))
     #print(arr)
-    return arr
+    return np.array(arr, dtype=object)
 
 
 def load_time(sub_idx, sign):
@@ -33,15 +48,16 @@ def main(sign):
         data.append(load_time(x, sign))
     arr = np.array(data)
     final = []
-    # print all the values of the start and end of repretitions person wise
-    for p in range(1,5):
+    # print all the values of the start and end of repetitions person wise
+    for p in range(1, 5):
         for k in range(0, 39, 2):
-            start = arr[p-1, k, 0]
-            end = arr[p-1, k + 1, 0]
-            final.append(load_rows(start, end, sign,p))
+            start = arr[p - 1, k]
+            end = arr[p - 1, k + 1]
+            final.append(load_rows(start, end, sign, p))
             # print(arr[0, k, 0],arr[0, k+1, 0])
-    check =  pd.DataFrame(final)
-    check.to_csv('finalCheck.csv')
+    #check = pd.DataFrame(final)
+    np.savetxt("data.csv",final,delimiter=", ",fmt='% s')
     #print(final)
+
 
 main(61)
